90914242876013dc820cb7deb046e2be81fabf3e
Small_sunshine <MCYXG2008@outlook.com>
2026-05-28 18:54:03 +0800

    fix(ui pages): 给主页内容组件传递上下文参数
    修复_homecontent组件无法获取正确上下文的问题，将当前页面的context传入组件中

--- lib/src/ui/pages/ba_main_page.dart
+++ lib/src/ui/pages/ba_main_page.dart
@@ -333,7 +333,7 @@ class _BAMCMainPageState extends State<BAMCMainPage> {
       case 3:
         return const BASettingsPage();
       default:
-        return _HomeContent();
+        return _HomeContent(mainContext: context);
     }
   }
 
